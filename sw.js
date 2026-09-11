6b8058863407a2bcd4f64b812c696d4857380942
kawa615 <kaba9531@gmail.com>
2026-09-10 23:20:55 -0500

    Refresh app cache for weekly report export

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="weekly-soccer-v3";
+const CACHE_NAME="weekly-soccer-v4";
 const APP_SHELL=["./","./index.html","./manifest.webmanifest","./icon.svg","./weekly-soccer-kick.jpg","./weekly-soccer-icon-192.png","./weekly-soccer-icon-512.png"];
 
 self.addEventListener("install",event=>{
